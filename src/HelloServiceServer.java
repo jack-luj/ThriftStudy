20ba5dafc778ed360b5cbe492fc18220479557b8
jack-luj <wjsc@qq.com>
2016-05-12 16:45:19 +0800

    init

---
+++ src/HelloServiceServer.java
@@ -0,0 +1,34 @@
+/**
+ * Created by jack lu on 2016/1/28.
+ */
+
+import org.apache.thrift.TProcessor;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TBinaryProtocol.Factory;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TServer.Args;
+import org.apache.thrift.server.TSimpleServer;
+import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.transport.TTransportException;
+
+public class HelloServiceServer {
+    /**
+     * 启动 Thrift 服务器
+     * @param args
+     */
+    public static void main(String[] args) {
+        try {
+            // 设置服务端口为 7911
+            TServerSocket serverTransport = new TServerSocket(7911);
+            // 设置协议工厂为 TBinaryProtocol.Factory
+            Factory proFactory = new TBinaryProtocol.Factory();
+            // 关联处理器与 Hello 服务的实现
+            TProcessor processor = new Hello.Processor(new HelloServiceImpl());
+            TServer server = new TSimpleServer(new Args(serverTransport).processor(processor));
+            System.out.println("Start server on port 7911...");
+            server.serve();
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        }
+    }
+}
